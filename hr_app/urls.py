f9346df3606c8ca9f9997ed9c438c644590b0076
coder-ravi100 <ravirathod100100@gmail.com>
2026-02-24 13:42:27 +0530

    back_page create and dashboard updated

--- hr_app/urls.py
+++ hr_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 
     path('hr_dashboard/', views.hr_dashboard, name='hr-dashboard'),
+    path('black_page/', views.black_page, name='black-page'),
 ]
--- hr_app/views.py
+++ hr_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def hr_dashboard(request):
     return render(request, 'hr_app/hr_dashboard.html')
+
+def black_page(request):
+    return render(request, 'hr_app/black_page.html')
