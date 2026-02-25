fed91a36d778d036e71c015212c5593796316bb0
coder-ravi100 <ravirathod100100@gmail.com>
2026-02-25 23:09:29 +0530

    create pages apply-leve,department,department-add,designation,add-designation,leve,leave-request,leave-balace

--- hr_app/urls.py
+++ hr_app/urls.py
@@ -6,5 +6,17 @@ urlpatterns = [
 
     path('dashboard/', views.hr_dashboard, name='hr-dashboard'),
     path('employee/', views.employee, name='employee'),
-    path('employee_add/', views.employee_add, name='employee-add')
+    path('employee_add/', views.employee_add, name='employee-add'),
+    
+    
+    path('department/', views.department, name='department'),
+    path('department_add/', views.department_add, name='department-add'),
+
+    path('designation/', views.designation, name='designation'),
+    path('designation_add/', views.designation_add, name='designation-add'),
+
+    path('leave_management/', views.leave_management, name='leave-management'),
+    path('apply_leave/', views.apply_leave, name='apply-leave'),
+    path('Leave_request/', views.Leave_request, name='leave-request'),
+    path('leave_balance/', views.leave_balance, name='leave-balance')
 ]
--- hr_app/views.py
+++ hr_app/views.py
@@ -2,10 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 def hr_dashboard(request):
-    return render(request, 'hr_app/dashboard.html')
+    return render(request, 'hr_app/Dashboard.html')
 
 def employee(request):
     return render(request, 'hr_app/Employee.html')
 
 def employee_add(request):
     return render(request, 'hr_app/Employee_Add.html')
+
+def department(request):
+    return render(request, 'hr_app/Department.html')
+
+def department_add(request):
+    return render(request, 'hr_app/Department_Add.html')
+
+def designation(request):
+    return render(request, 'hr_app/Designation.html')
+
+def designation_add(request):
+    return render(request, 'hr_app/Designation_Add.html')
+
+def leave_management(request):
+    return render(request, 'hr_app/Leave_management.html')
+
+def apply_leave(request):
+    return render(request, 'hr_app/Apply_leave.html')
+
+def Leave_request(request):
+    return render(request, 'hr_app/Leave_request.html')
+
+def leave_balance(request):
+    return render(request, 'hr_app/Leave_balance.html')
