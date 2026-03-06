18416dca721ef7f0ea4ce986f8ae02f626539aa1
coder-ravi100 <ravirathod100100@gmail.com>
2026-03-06 21:41:42 +0530

    update

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+def login_view(request):
+
+    form = LoginForm()
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+
+            login(request, user)
+
+            role = user.role.name
+
+            if role == "Super Admin":
+                return render(request,"superadmin_dashboard")
+
+            elif role == "HR":
+                return render(request,"employee/HR/hr-dashboard")
+
+            elif role == "Manager":
+                return render(request,"manager_dashboard")
+
+            elif role == "Employee":
+                return render(request,"employee_dashboard")
+
+        else:
+            print("Invalid credentials")
+
+    return render(request,"accounts/login.html",{"form":form})
+        
